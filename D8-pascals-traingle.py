ad224fe83fbbead16dde47f042db1f8f719de003
sahibpreetsingh12 <ss9334931@gmail.com>
2025-04-28 01:52:21 -0400

    pascals traingle

---
+++ D8-pascals-traingle.py
@@ -0,0 +1,56 @@
+
+
+"""
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+
+https://leetcode.com/problems/pascals-triangle/
+"""
+from typing import List
+class Solution:
+    # recurssion solution
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
+        
+        for i in range(1, numRows - 1):
+            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+        
+        prevRows.append(newRow)
+        return prevRows
+
+
+"""
+Simpler Solution
+"""
+
+from typing import List
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+            triangle.append(row)
+
+        return triangle
