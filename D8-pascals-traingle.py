6cffb0cfd23af3d3763ca63fff2dce82f65af31c
sahibpreetsingh12 <ss9334931@gmail.com>
2025-04-30 19:17:59 -0400

    pascals traingle solved on own

--- D8-pascals-traingle.py
+++ D8-pascals-traingle.py
@@ -17,22 +17,51 @@ Output: [[1]]
 https://leetcode.com/problems/pascals-triangle/
 """
 from typing import List
+
+"""
+My own solution
+"""
 class Solution:
-    # recurssion solution
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0:
-            return []
-        if numRows == 1:
-            return [[1]]
+        # Initialize the final result list which will hold all rows of Pascal's Triangle
+        big_ls = []
+
+        # Loop through each row index from 1 to numRows (inclusive)
+        for i in range(1, numRows + 1):
+            # Start every row with 'i' elements initialized to 1
+            # In Pascal's Triangle, the first and last elements of each row are always 1
+            row = [1] * i
+
+            # Fill in the values between the first and last element for rows beyond the second
+            for j in range(1, i - 1):
+                # Compute the value based on the sum of two values from the previous row
+                # Specifically, row[j] = previous_row[j] + previous_row[j-1]
+                # We use i-2 because 'big_ls' is 0-indexed and we're currently at row 'i'
+                row[j] = big_ls[i - 2][j] + big_ls[i - 2][j - 1]
+
+            # Append the completed row to the result list
+            big_ls.append(row)
+
+        # Return the list of lists containing Pascal's Triangle up to numRows
+        return big_ls
+
         
-        prevRows = self.generate(numRows - 1)
-        newRow = [1] * numRows
+# class Solution:
+#     # recurssion solution
+#     def generate(self, numRows: int) -> List[List[int]]:
+#         if numRows == 0:
+#             return []
+#         if numRows == 1:
+#             return [[1]]
         
-        for i in range(1, numRows - 1):
-            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+#         prevRows = self.generate(numRows - 1)
+#         newRow = [1] * numRows
         
-        prevRows.append(newRow)
-        return prevRows
+#         for i in range(1, numRows - 1):
+#             newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+        
+#         prevRows.append(newRow)
+#         return prevRows
 
 
 """
