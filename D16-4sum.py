4c98e56d780ab5a95e91ee0aac7f811aef9532d6
sahibpreetsingh12 <ss9334931@gmail.com>
2025-05-07 21:17:08 -0400

    4sum problem solved

--- D16-4sum.py
+++ D16-4sum.py
@@ -19,7 +19,47 @@ Output: [[2,2,2,2]]
 
 Leetcode -  https://leetcode.com/problems/4sum/
 """
-
+class Solution:         
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        result = []
+        n = len(nums)
+        
+        # Skip duplicate first elements
+        for i in range(n-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+            # Skip duplicate second elements
+            for j in range(i+1, n-2):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                    
+                # Use two pointers for the remaining two elements
+                left = j + 1
+                right = n - 1
+                
+                while left < right:
+                    curr_sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    
+                    if curr_sum == target:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        
+                        # Skip duplicates
+                        while left < right and nums[left] == nums[left+1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -= 1
+                            
+                        left += 1
+                        right -= 1
+                        
+                    elif curr_sum < target:
+                        left += 1
+                    else:
+                        right -= 1
+                        
+        return result
 """
 solution which didnt work -- Its correct but not efficient for time
 
